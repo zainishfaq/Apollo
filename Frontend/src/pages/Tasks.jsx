6af68a9415c779904f1f5514c23bde897e0d0c30
zainishfaq <zainishfaq4@gmail.com>
2024-07-11 07:06:26 -0700

    Task Main Content page created

--- Frontend/src/pages/Tasks.jsx
+++ Frontend/src/pages/Tasks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaFilter, FaTasks, FaCalendarAlt, FaUser, FaStar } from 'react-icons/fa';
+//import { FaFilter, FaTasks, FaCalendarAlt, FaUser, FaStar } from 'react-icons/fa';
+import { FaPlay, FaClock, FaUser, FaChevronDown, FaEllipsisV } from 'react-icons/fa';
 import { FaSearch } from 'react-icons/fa';
 import TaskStatus from '../components/Filters/TaskStatus';
 import { GoSearch } from 'react-icons/go';
@@ -119,17 +120,35 @@ function Tasks() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 p-4">
-        <div className="flex justify-between items-center mb-4">
-          <button className="flex items-center bg-gray-200 p-2 rounded">
+      <div className="flex-1 bg-white p-4">
+        <div className="flex items-center mb-4">
+          <button className="flex text-sm w-28 h-8 items-center bg-white text-black border border-black p-2 rounded">
             <FaSearch className="mr-2" />
             Hide Filters
           </button>
-          <div className="flex items-center">
-            <span className="mr-2">0 Total</span>
-          </div>
+            <button className='bg-blue-100 w-24 h-12 m-0 p-0 ml-2'> <span>0</span><br/>Total</button>
         </div>
         <div className="border-t border-gray-200 pt-4">
+          <div className='flex flex-row'>
+          <input
+                type="checkbox"
+                id="dropdownCheckbox"
+                className="p-2 pt-0"
+            />
+            <div className='ml-14 mt-1'>
+              <button className='border border-gray-400 p-2'><FaPlay className=" text-sm text-gray-400" /></button>
+              <button className='border border-gray-400 p-2'><FaClock className=" text-sm text-gray-400" /></button>
+              <button className='border border-gray-400 p-2'><FaUser className=" text-sm text-gray-400" /></button>
+              <button className='border border-gray-400 p-2'><FaChevronDown className=" text-sm text-gray-400"/></button>
+              <button className='border border-gray-400 p-2'> <FaEllipsisV className=" text-sm text-gray-400" /></button>
+              
+            </div>
+            <button className='ml-96 mt-1 w-32 h-8 border border-gray-400'>
+              Due Date
+            </button>
+            <button className='border border-gray-400 p-2 h-8 mt-1'><FaClock className=" text-sm text-gray-400" />
+            </button>
+          </div>
           <div className="flex justify-between items-center mb-2">
             <span>Task</span>
             <span>Associated with</span>
